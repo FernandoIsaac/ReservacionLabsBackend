52abf965fbd3eaa541ed883d4027e05e59e4794a
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-18 23:48:55 -0600

    Arreglado error en las rutas con el register

--- backend/routes.js
+++ backend/routes.js
@@ -8,7 +8,7 @@ var recursoController = require('./controllers/recursoController');
 
 exports.endpoints = [{method: 'GET', path: '/', config: {handler: function(request, reply){reply('API v1, LABS')}}},
 
-	{method: 'POST', path: '/v1/register', config: adminuserController.createUser},
+	{method: 'POST', path: '/v1/register', config: adminuserController.createDocente},
 	{method: 'POST', path: '/v1/login', config: authController.login},
 	{method: 'GET', path: '/v1/logout', config: authController.logout},
 
