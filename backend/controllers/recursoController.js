f1029a41fc706a6c92f33d2284733b30d2938d23
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-20 12:03:17 -0600

    Recurso ya hace add, edit y delete en postgresql :D

--- backend/controllers/recursoController.js
+++ backend/controllers/recursoController.js
@@ -12,8 +12,27 @@ exports.listRecurso = {
 
 exports.getRecurso = {
   handler: function(request, reply){
-    var recurso = recurso.find({_id:request.params.recursoId});
-    reply(recurso);
+    var recurso = [];
+
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+          done();
+          console.log(err);
+          return reply.status(500).json({ success: false, data: err});
+        }
+
+        var query = client.query("SELECT * FROM Recurso Where id_Recurso = $1 ",[request.params.recursoId]);
+
+        query.on('row', function(row) {
+            recurso.push(row);
+        });
+
+        query.on('end', function() {
+            done();
+            return reply(recurso);
+        });
+
+    });
     console.log('get Recurso success')
   }
 }
@@ -21,23 +40,49 @@ exports.getRecurso = {
 exports.addRecurso = {
   handler: function(request, reply){
     console.log("Entra aqui a addRecurso");
-    client.query("INSERT INTO Recurso VALUES(request.payload.id_Recurso, request.payload.nombre, request.payload.descripcion)");
+
+    pg.connect(conString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return reply.status(500).json({ success: false, data: err});
+        }
+        client.query("INSERT INTO Recurso values($1, $2, $3)", [request.payload.id_Recurso, request.payload.nombre, request.payload.descripcion]);
+
+    });
+
     reply("Recurso agregado")
   }
 }
 
 exports.editRecurso = {
   handler: function(request, reply){
-    recurso.update({_id : request.params.recursoId},
-      {id_Recurso: {type: String, unique: true, required: true},
-      nombre: String,
-      descripcion: String,
-      recursos:[String],}).exec();
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+          done();
+          console.log(err);
+          return reply.status(500).json({ success: false, data: err});
+        }
+
+        client.query("UPDATE Recurso SET nombre = ($1), descripcion = ($2) WHERE id_Recurso = ($3)", [request.payload.nombre, request.payload.descripcion, request.params.recursoId]);
+
+    });
       reply("Recurso edited")
 }}
 
 exports.removeRecurso = {
   handler: function(request, reply){
-    recurso.find({_id: request.params.recursoId}).remove().exec();
+
+        pg.connect(conString, function(err, client, done) {
+            if(err) {
+              done();
+              console.log(err);
+              return reply.status(500).json({ success: false, data: err});
+            }
+
+            client.query("DELETE FROM Recurso WHERE id_Recurso = ($1)", [request.params.recursoId]);
+
+        });
     reply("Recurso removed");
 }}
--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ client.connect();
 //client.query("CREATE TABLE Docente ( email char(20), password char(20), primerNombre char(30), segundoNombre char(30), primerApellido char(30), segundoApellido char(30), campus char(30), departamento char(30), telefono char(20), scope char(20), PRIMARY KEY (email))");
 //client.query("CREATE TABLE Laboratorio ( id_Laboratorio char(15), nombre char(20), desripcion char(30), ubicacion char(20), capacidad numeric(9,0), PRIMARY KEY (id_Laboratorio))");
 //client.query("CREATE TABLE Reservacion ( id_Reservacion char(15), descripcion char(30), estado char(20), fecha_inicial date, fecha_final date, hora_inicial time(7), hora_final time(7), email_Docente char(20) REFERENCES Docente(email), id_Laboratorio char(15) REFERENCES Laboratorio(id_Laboratorio), PRIMARY KEY (id_Reservacion))");
-//client.query("CREATE TABLE Recurso ( id_Recurso char(15), nombre char(20), desripcion char(30), PRIMARY KEY (id_Recurso))");
+//client.query("CREATE TABLE Recurso ( id_Recurso char(15), nombre char(20), descripcion char(30), PRIMARY KEY (id_Recurso))");
 //client.query("CREATE TABLE Recursos_de_Laboratorio ( id_Laboratorio char(15) REFERENCES Laboratorio(id_Laboratorio), id_Recurso char(15) REFERENCES Recurso(id_Recurso))");
 
 server.register([inert, auth], function(err){
