8e6b226cdad7eaf768fcc854d07894bb133c6b35
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-20 19:30:08 -0600

    Deletear los schemas

--- backend/schemas/recurso.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var RecursoSchema = new mongoose.Schema({
-  id_Recurso: {type: String, unique: true, required: true},
-  nombre: String,
-  descripcion: String,
-});
-
-module.exports = mongoose.model('Recurso', RecursoSchema);
